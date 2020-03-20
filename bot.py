d0087fa5484e6c4069399cd9c1c0c9e20a1ceec3
Tawanda M. Nyoni <hkay.tee@outlook.com>
2020-03-19 17:09:59 -0700

    connected to the Mongodb

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ import space as space
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
-    async def on_members_added_activity(self, members_added: ChannelAccount, turn_context: TurnContext ):
+    async def on_members_added_activity(self, members_added: ChannelAccount, turn_context: TurnContext):
         for member_added in members_added:
             if member_added.id != turn_context.activity.recipient.id:
                 await turn_context.send_activity(
@@ -28,14 +28,21 @@ class MyBot(ActivityHandler):
         response.suggested_actions = SuggestedActions(
             actions=[
                 CardAction(title="1. Space", type=ActionTypes.im_back, value="space"),
-                CardAction(title="2. Community", type=ActionTypes.im_back, value="community")
+                CardAction(title="2. Community", type=ActionTypes.im_back, value="community"),
+                CardAction(title="3. Inquiries", type=ActionTypes.im_back, value="inquires")
             ]
         )
         return await turn_context.send_activity(response)
+
+
+    async def space_services(self, turn_context: TurnContext):
+        basic_space = space.basic_space()  
+        await turn_context.send_activity('For Co-Working Spaces the following is available: \n 1. %s which costs %s' % (str(basic_space['title']), str(basic_space['cost'])))  
+        return await self.services_offered(turn_context)
 
     async def on_message_activity(self, turn_context: TurnContext):
         user_response = turn_context.activity.text.lower
         if user_response is 'space':
             await turn_context.send_activity(f'This is our space, What do you want to know about our space?')
         
-        return await self.services_offered(turn_context)
+        return await self.space_services(turn_context)
